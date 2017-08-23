2fa3ae41243870735cb0df5c093cf143cfb4ccce
YNZ <YNZ@192.168.8.104>
2017-08-23 15:02:15 +0200

    file path;

--- src/File/Image2Array.java
+++ src/File/Image2Array.java
@@ -7,12 +7,11 @@ package File;
 
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
+import javax.imageio.ImageIO;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.imageio.ImageIO;
 
 /**
  *
@@ -26,10 +25,12 @@ public class Image2Array {
         File file = new File("./src/tmp", "hit.jpg");
         try {
             image = ImageIO.read(file);
-            ByteArrayOutputStream baos = new ByteArrayOutputStream(1024);
-            System.out.println("write to buffer" + ImageIO.write(image, "jpg", baos));
-            imageArray = baos.toByteArray();
-            System.out.println("size of array " + imageArray.length);
+            try (ByteArrayOutputStream baos = new ByteArrayOutputStream(1024)) {
+                System.out.println("write to buffer" + ImageIO.write(image, "jpg", baos));
+                imageArray = baos.toByteArray();
+                System.out.println("size of array " + imageArray.length);
+                baos.flush();
+            }
 
         } catch (IOException ex) {
             Logger.getLogger(Image2Array.class.getName()).log(Level.SEVERE, null, ex);

6262598c9be48700959aa4e9e01597eb870a3455
YNZ <YNZ@192.168.8.104>
2017-08-26 19:16:44 +0200

    file input stream read integer, end at -1; write int to output stream

---
+++ src/FileIO/CopyPdfUsingStream.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FileIO;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author YNZ
+ */
+public class CopyPdfUsingStream {
+
+    public static void main(String[] args) {
+
+        String name = "billet.pdf";
+        String copy = "copyBillet.pdf";
+
+        try (
+                FileInputStream fis = new FileInputStream(name);
+                FileOutputStream fos = new FileOutputStream(copy);) {
+
+            int data;
+            while ((data = fis.read()) != -1) {
+                fos.write(data);
+            }
+
+        } catch (IOException ex) {
+            Logger.getLogger(CopyPdfUsingStream.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
